6c69685aed46ac9e48f10740cd28444a731b01b0
bredbk <bredbk@gmail.com>
2022-07-02 21:40:22 -0300

    finalizando Users do controller

--- app/api/controllers/users.js
+++ app/api/controllers/users.js
@@ -9,6 +9,24 @@ module.exports = {
              next(err);
             else
              res.json({status: "success", message: "User added successfuly!", data: null});
-        })
-    }
+            });
+        },
+
+        authenticate: function(req, res, next) {
+            userModel.findOne({email: req.body.email}, function(err, userInfo){
+                if(err){
+                    next(err);
+                }else{
+                    if(bcrypt.compareSync(req.body.password, userInfo.password)){
+                        const token = jwt.sign({id: userInfo._id}, reqapp.get('secretKey'),{
+                            expiresIn: '1h'});
+                        res.json({status: "success", message: "User found!", data: {user: userInfo, token: token}});
+                    }else{
+                        res.json({status: "error", message: "Invalid email/password!", data:null});
+
+                    }
+                }
+            });
+        }
+    
 }
