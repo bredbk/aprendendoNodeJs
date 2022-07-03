89d52f4e81238afa0b4a0db728ba0371d4a7bf33
bredbk <bredbk@gmail.com>
2022-07-02 21:59:51 -0300

    criado routes user

---
+++ routes/users.js
@@ -0,0 +1,8 @@
+const espress = require('express');
+const router = express.Router();
+const userController = require('../controllers/users');
+
+router.post('/register', userController.create);
+router.post('/authenticate', userController.authenticate);
+
+module.exports = router;
